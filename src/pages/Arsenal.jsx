2a5bff10d372b397f393017ca5f7fe4b9883004f
daveesin <davisilvagoncalves0208@gmail.com>
2026-08-11 15:11:13 -0300

    Start the component to return something just to configure the path in the router

---
+++ src/pages/Arsenal.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
+
+function Arsenal() {
+    return(
+        <div>
+
+        <Navbar />
+        <h1>Arsenal</h1>
+        <Footer />
+
+        </div>
+    )
+}
+
+export default Arsenal;
